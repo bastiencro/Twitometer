2030a67da620e3db38901b00aaff5244c3ff0736
rackoss <crozet.bastien1@gmail.com>
2017-11-14 14:22:46 +0100

    Added reply with media
    plus example in essais.js

--- essais.js
+++ essais.js
@@ -1,4 +1,4 @@
-/*var Twit = require('twit')
+var Twit = require('twit')
 var fs = require('fs')
 
 var T = new Twit({
@@ -8,12 +8,36 @@ var T = new Twit({
   access_token_secret:  'X5bwtrbnAFAg6mSCdGhJgRYaCiDSrIm0VDOMj7u9b67An',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
-
+/*
 T.post('statuses/update', { status: 'ananas' }, function(err, data, response) {
   console.log(data)
 })
 */
-var five = require("johnny-five");
+var b64content = fs.readFileSync('D:\\Documents\\GitHub\\Twitometer\\gif\\lap_red.gif', { encoding: 'base64' })
+
+// first we must post the media to Twitter
+T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+  // now we can assign alt text to the media, for use by screen readers and
+  // other text-based presentations and interpreters
+  var mediaIdStr = data.media_id_string
+  var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+  T.post('media/metadata/create', meta_params, function (err, data, response) {
+    if (!err) {
+      // now we can reference the media and post a tweet (media will attach to the tweet)
+      var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+
+      T.post('statuses/update', params, function (err, data, response) {
+        console.log(data)
+      })
+    }
+  })
+})
+
+
+
+/*var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
@@ -26,4 +50,4 @@ board.on("ready", function() {
   this.wait(500, function() {
     led.fadeOut();
   });
-});
+});*/
--- twitometer.js
+++ twitometer.js
@@ -23,7 +23,6 @@ var RHONE = 1; //var globale
 var SAONE = 2;
 
 var stream = T.stream('statuses/filter', { track: '#test_ixda18_1' })
-
 stream.on('tweet', function (tweet) {
     //console.log(tweet)
     process_tweet(tweet)
@@ -31,7 +30,7 @@ stream.on('tweet', function (tweet) {
 
 
   function process_tweet( tweet ) {
-    console.log(tweet)
+    //console.log(tweet)
     //console.log(tweet.id_str)
     var id = tweet.id;
 
@@ -49,17 +48,39 @@ stream.on('tweet', function (tweet) {
 
     if (scoreboard[tweet.user.name].score % 5 == 0) {
     console.log("score 1");
-      T.post('statuses/update', {
-        status: '@'+tweet.user.screen_name+' Merci !',
-        in_reply_to_status_id : tweet.id,
-        in_reply_to_user_id : tweet.user.id,
-      }, function(err, data, response) {
+    var b64content = fs.readFileSync('D:\\Documents\\GitHub\\Twitometer\\gif\\lap_red.gif', { encoding: 'base64' })
+
+    // first we must post the media to Twitter
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and
+      // other text-based presentations and interpreters
+      var mediaIdStr = data.media_id_string
+      var altText = "You completed an entire lap, congrats !"
+      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+      T.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (!err) {
+          // now we can reference the media and post a tweet (media will attach to the tweet)
+          var params = { status: '@'+tweet.user.screen_name+' You completed a lap !', media_ids: [mediaIdStr] }
+
+          T.post('statuses/update', params, function (err, data, response) {
+            console.log(data)
+          })
+        }
+      })
+    })
+  }
+
+        //status: '@'+tweet.user.screen_name+' Merci !',
+        //in_reply_to_status_id : tweet.id,
+        //in_reply_to_user_id : tweet.user.id,
+      //}, function(err, data, response) {
 
                 //console.log(data)
                         //console.log(err)
                                 //console.log(response)
-      })
-    }
+      //})
+    //}
 
 
     if (team === 1) {
